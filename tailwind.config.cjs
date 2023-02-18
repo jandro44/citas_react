2627edebbad8cd920a7a3576f3f0cd1589e0fd2e
Alejandro <castropintoale@gmail.com>
2023-02-18 01:01:13 -0300

    primer commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"], // con los asteriscos le decimos que abarque todos los archivos que esten dentro de src y todos los archivos jsx
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
